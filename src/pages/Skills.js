36af357962f8dd082500a35b624ead53d682de93
Elaf <skelaf2000@gmail.com>
2023-11-20 21:05:54 +0530

    Skills page complete

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -27,48 +27,68 @@ const Skills = () => {
   };
   return (
     <>
-      <h2 className='text-center text-3xl underline m-4'>Skills</h2>
-      <div className='flex flex-wrap justify-center items-center space-x-2 bg-gray-700 p-4 md:p-8'>
-        <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleHTML}>
-          <img src="/icons/html.svg" alt="html" className='h-28 w-28 mx-auto' />
-          <p className='text-center'>HTML</p>
+      <div>
+
+        <h2 className='text-center text-3xl underline m-4'>Skills</h2>
+        <div className='flex flex-wrap justify-center items-center space-x-2 bg-gray-700 p-4 md:p-8'>
+          <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleHTML}>
+            <img src="/icons/html.svg" alt="html" className='h-28 w-28 mx-auto' />
+            <p className='text-center'>HTML</p>
+          </div>
+          <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleExpress}>
+            <img src="/icons/css.svg" alt="css" className='h-28 w-28 mx-auto' />
+            <p className='text-center'>CSS</p>
+          </div>
+          <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleJS}>
+            <img src="/icons/javascript.svg" alt="javascript" className='h-28 w-28 mx-auto' />
+            <p className='text-center'>JavaScript</p>
+          </div>
+          <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleReact}>
+            <img src="/icons/react.svg" alt="react" className='h-28 w-28 mx-auto' />
+            <p className='text-center'>React Js</p>
+          </div>
+          <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleNode}>
+            <img src="/icons/nodejs.svg" alt="work in progress" className='h-28 w-28 mx-auto' />
+            <p className='text-center'>Node Js</p>
+          </div>
+          <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleExpress}>
+            <img src="/icons/express.svg" alt="work in progress" className='h-28 w-28 mx-auto' />
+            <p className='text-center'>Express Js</p>
+          </div>
+          <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleExpress}>
+            <img src="/icons/mongodb.svg" alt="work in progress" className='h-28 w-28 mx-auto' />
+            <p className='text-center'>MongoDB</p>
+          </div>
+          <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleJava}>
+            <img src="/icons/java.svg" alt="work in progress" className='h-28 w-28 mx-auto' />
+            <p className='text-center'>Java</p>
+          </div>
+          <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleTailwind}>
+            <img src="/icons/tailwind.svg" alt="work in progress" className='h-28 w-28 mx-auto' />
+            <p className='text-center'>Tailwind CSS</p>
+          </div>
+          <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleGit}>
+            <img src="/icons/github.svg" alt="work in progress" className='h-28 w-28 mx-auto' />
+            <p className='text-center'>GitHub</p>
+          </div>
         </div>
-        <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleExpress}>
-          <img src="/icons/css.svg" alt="css" className='h-28 w-28 mx-auto' />
-          <p className='text-center'>CSS</p>
-        </div>
-        <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleJS}>
-          <img src="/icons/javascript.svg" alt="javascript" className='h-28 w-28 mx-auto' />
-          <p className='text-center'>JavaScript</p>
-        </div>
-        <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleReact}>
-          <img src="/icons/react.svg" alt="react" className='h-28 w-28 mx-auto' />
-          <p className='text-center'>React Js</p>
-        </div>
-        <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleNode}>
-          <img src="/icons/nodejs.svg" alt="work in progress" className='h-28 w-28 mx-auto' />
-          <p className='text-center'>Node Js</p>
-        </div>
-        <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleExpress}>
-          <img src="/icons/express.svg" alt="work in progress" className='h-28 w-28 mx-auto' />
-          <p className='text-center'>Express Js</p>
-        </div>
-        <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleExpress}>
-          <img src="/icons/mongodb.svg" alt="work in progress" className='h-28 w-28 mx-auto' />
-          <p className='text-center'>MongoDB</p>
-        </div>
-        <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleJava}>
-          <img src="/icons/java.svg" alt="work in progress" className='h-28 w-28 mx-auto' />
-          <p className='text-center'>Java</p>
-        </div>
-        <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleTailwind}>
-          <img src="/icons/tailwind.svg" alt="work in progress" className='h-28 w-28 mx-auto' />
-          <p className='text-center'>Tailwind CSS</p>
-        </div>
-        <div className='border-2 border-indigo-600 p-6 rounded-md mb-4 md:mb-4' style={shadowStyleGit}>
-          <img src="/icons/github.svg" alt="work in progress" className='h-28 w-28 mx-auto' />
-          <p className='text-center'>GitHub</p>
-        </div>
+          <div>
+            <h2 className='text-center text-3xl underline m-4'>Interest</h2>
+            <div className='flex flex-wrap justify-center md:space-x-44 bg-gray-700 p-4 md:p-8'>
+              <div className='border-2 border-white p-6 rounded-md mb-4 md:mb-4'>
+                <img src="/icons/blockchain.svg" alt="blockchain" className='h-28 w-28 mx-auto' />
+                <p className='text-center pt-2'>Blockchain</p>
+              </div>
+              <div className='border-2 border-white p-6 rounded-md mb-4 md:mb-4'>
+                <img src="/icons/finance.svg" alt="finance" className='h-28 w-28 mx-auto' />
+                <p className='text-center pt-2'>Finance</p>
+              </div>
+              <div className='border-2 border-white p-6 rounded-md mb-4 md:mb-4'>
+                <img src="/icons/article.svg" alt="article" className='h-28 w-28 mx-auto' />
+                <p className='text-center pt-2'>Article Writing</p>
+              </div>
+            </div>
+          </div>
       </div>
     </>
   )
